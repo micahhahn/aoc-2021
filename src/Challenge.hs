6a0f7fe139fd2f8fcb58cc689dd4222335695338
Micah Hahn <micah@noredink.com>
2021-12-05 17:38:47 -0500

    Add Day3

--- src/Challenge.hs
+++ src/Challenge.hs
@@ -5,6 +5,7 @@ module Challenge
 
 import qualified Day1
 import qualified Day2
+import qualified Day3
 
 data Challenge = Challenge 
     { _part1 :: [String] -> String
@@ -19,4 +20,5 @@ challenges :: [Challenge]
 challenges = 
     [ Challenge Day1.part1 Day1.part2 "day-1.txt" Day1.example "7" "5"
     , Challenge Day2.part1 Day2.part2 "day-2.txt" Day2.example "150" "900"
+    , Challenge Day3.part1 Day3.part2 "day-3.txt" Day3.example "" ""
     ]
---
+++ src/Day3.hs
@@ -0,0 +1,22 @@
+module Day3 (example, part1, part2) where
+
+example = 
+    [ "00100"
+    , "11110"
+    , "10110"
+    , "10111"
+    , "10101"
+    , "01111"
+    , "00111"
+    , "11100"
+    , "10000"
+    , "11001"
+    , "00010"
+    , "01010"
+    ]
+
+part1 :: [String] -> String
+part1 = const ""
+
+part2 :: [String] -> String
+part2 = const ""
