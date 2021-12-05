d0990906ecc7464a0f120e023029cb2ca2b8620a
Micah Hahn <micah@noredink.com>
2021-12-05 14:37:45 -0500

    1.2 Solution

--- day-1/Main.hs
+++ day-1/Main.hs
@@ -1,21 +1,30 @@
 module Main where
 
+import Data.Functor ((<&>))
 import Lib
 import System.IO
 
 import qualified Paths_aoc as Paths
 
-loadLines = Paths.getDataFileName "day-1/input.txt" >>= readFile >>= pure . lines
+loadValues :: IO [Int]
+loadValues = Paths.getDataFileName "day-1/input.txt" >>= readFile <&> fmap (read :: String -> Int) . lines
 
-lesser :: [String] -> [(Int, Int)]
-lesser lines = [(l, r) | (l, r) <- zip values (drop 1 values), l < r]
-    where values = fmap (read :: String -> Int) lines
+lesserPairs :: [Int] -> [(Int, Int)]
+lesserPairs values = [(l, r) | (l, r) <- zip values (drop 1 values), l < r]
 
-part1 :: IO Int
-part1 = do
-    fileLines <- loadLines
-    let result = length $ lesser fileLines
-    return result
+part1 :: [Int] -> Int
+part1 values =
+    let result = length $ lesserPairs values
+     in result
 
-main :: IO Int
-main = part1
+part2 :: [Int] -> Int
+part2 values =
+    let tripletSums = [ v1 + v2 + v3 | (v1, v2, v3) <- zip3 values (drop 1 values) (drop 2 values) ]
+     in length $ lesserPairs tripletSums
+
+main :: IO (Int, Int)
+main = do
+    values <- loadValues
+    let result1 = part1 values
+    let result2 = part2 values
+    return (result1, result2)
